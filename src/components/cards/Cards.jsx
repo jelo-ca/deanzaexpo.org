563c894ae69a418b1b54219b447d6c3242742955
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2025-12-20 21:45:09 -0800

    card integration

--- src/components/cards/Cards.jsx
+++
@@ -1,6 +0,0 @@
-import { getSpeakers } from "../../lib/apiSpeakers";
-
-export default function Card({ data }) {
-  data = getSpeakers();
-  console.log(data);
-}
--- src/sections/speakers/Speakers.jsx
+++ src/sections/speakers/Speakers.jsx
@@ -1,11 +1,33 @@
-import Card from "../../components/cards/Cards";
+import Card from "../../components/card/Card";
+import { useState, useEffect } from "react";
+import { getSpeakers } from "../../lib/apiSpeakers";
+import "./Speakers.css"
+
 
 export default function Speakers() {
+
+    const [speakers, setSpeakers] = useState([]);
+    const [err, setErr] = useState("");
+
+    async function refresh() {
+  setErr("");
+
+  const res = await getSpeakers();
+
+  if (res?.error) throw res.error;
+
+  setSpeakers(res);
+}
+
+  useEffect(() => {
+      refresh().catch((e) => setErr(e.message));
+    }, []);
+
   return (
     <section id="speakers-section">
       <h2>Speakers</h2>
       <div className="speaker-carousel">
-        <Card />
+        {speakers.map((s) => <Card key={s.id} data={s}/>)}
       </div>
     </section>
   );
