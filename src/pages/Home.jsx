fe890451d13b0c305983d97e4e2bd8932060080f
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2025-12-21 22:15:50 -0800

    updated data passing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,8 @@
+import { useState, useEffect } from "react";
+import { getSpeakers } from "../lib/apiSpeakers.js";
+import { getProjects } from "../lib/apiProjects.js";
+
+// SECTIONS
 import Hero from "../sections/hero/Hero";
 import About from "../sections/about/About";
 import Speakers from "../sections/speakers/Speakers";
@@ -6,12 +11,33 @@ import Sponsors from "../sections/sponsors/Sponsors";
 import FAQ from "../sections/faq/FAQ";
 
 export default function Home() {
+  const [speakers, setSpeakers] = useState([]);
+  const [projects, setProjects] = useState([]);
+  const [err, setErr] = useState("");
+
+  async function refresh() {
+    setErr("");
+
+    const s = await getSpeakers();
+    const p = await getProjects();
+
+    if (s?.error) throw s.error;
+    if (p?.error) throw p.error;
+
+    setSpeakers(s);
+    setProjects(p);
+  }
+
+  useEffect(() => {
+    refresh().catch((e) => setErr(e.message));
+  }, []);
+
   return (
     <>
       <Hero />
       <About />
-      <Speakers />
-      <Projects />
+      <Speakers speakerData={speakers} />
+      <Projects projectData={projects} />
       <Sponsors />
       <FAQ />
       <footer>
--- src/sections/speakers/Speakers.jsx
+++ src/sections/speakers/Speakers.jsx
@@ -1,31 +1,12 @@
 import Card from "../../components/card/Card";
-import { useState, useEffect } from "react";
-import { getSpeakers } from "../../lib/apiSpeakers";
 import "./Speakers.css";
 
-export default function Speakers() {
-  const [speakers, setSpeakers] = useState([]);
-  const [err, setErr] = useState("");
-
-  async function refresh() {
-    setErr("");
-
-    const res = await getSpeakers();
-
-    if (res?.error) throw res.error;
-
-    setSpeakers(res);
-  }
-
-  useEffect(() => {
-    refresh().catch((e) => setErr(e.message));
-  }, []);
-
+export default function Speakers({ speakerData }) {
   return (
     <section id="speakers-section">
       <h2>Speakers</h2>
       <div className="speaker-carousel">
-        {speakers.map((s) => (
+        {speakerData.map((s) => (
           <Card key={s.id} data={s} type="speaker" />
         ))}
       </div>
