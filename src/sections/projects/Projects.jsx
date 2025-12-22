57de9a81abf8c066ecf19ddf5b43eaacf0f8c00b
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2025-12-22 12:09:38 -0800

    Minor carousel fix

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -1,7 +1,14 @@
-export default function Projects() {
+import Card from "../../components/card/Card";
+
+export default function Projects({ projectData }) {
   return (
     <section id="projects-section">
       <h2>Projects</h2>
+      <div className="project-container">
+        {projectData.map((p) => (
+          <Card id={p.id} data={p} type="project" />
+        ))}
+      </div>
     </section>
   );
 }
