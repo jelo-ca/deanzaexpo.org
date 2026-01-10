f9e499c61b94779e9c73b149371ae85820a50292
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2026-01-09 20:10:21 -0800

    temp sponsor fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import "./Animation.css";
 import Home from "./pages/Home";
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
