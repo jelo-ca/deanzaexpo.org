73972795cc245525bcfcb1c8f3e5285129aa95cb
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2026-01-16 19:09:10 -0800

    updated anim

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,11 +1,19 @@
 import "./Button.css";
+import { motion } from "framer-motion";
 
 export default function Button({ label, width, href }) {
   if (href) {
     return (
-      <a href={href} className="btn-container" target="_blank"> 
+      <motion.a
+        href={href}
+        className="btn-container"
+        target="_blank"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
         {label}
-      </a>
+      </motion.a>
     );
   } else {
     return <button className="btn-container">{label}</button>;
