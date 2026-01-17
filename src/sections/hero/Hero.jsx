6ddc82122fd95610b90e3f58be10699f434865a2
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2026-01-16 19:09:10 -0800

    updated anim

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -1,16 +1,25 @@
 import "./Hero.css";
 import Button from "../../components/button/Button";
-
+import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
     <section id="hero-section">
       <img class="hero-background" src="expo.jpg" alt="" />
       <div class="overlay">
-        <h2 class="tagline">
+        <motion.h2
+          class="tagline"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
           Innovation expo for the students, by the students
-        </h2>
-        <Button label="Register Now" width={150} href={"https://www.eventbrite.com/e/1978756589225?aff=oddtdtcreator"} />
+        </motion.h2>
+        <Button
+          label="Register Now"
+          width={150}
+          // href={"https://www.eventbrite.com/e/1978756589225?aff=oddtdtcreator"}
+        />
       </div>
     </section>
   );
