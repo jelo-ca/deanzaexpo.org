39ce3a9bd19d116c2a80ff2d6a40ddd7ba3205fb
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2026-01-18 13:36:07 -0800

    content fix

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -33,15 +33,15 @@ export default function Footer() {
             <li>
               <a href="#about-section">About</a>
             </li>
-            <li>
+            {/* <li>
               <a href="#speakers-section">Speakers</a>
-            </li>
+            </li> */}
             <li>
               <a href="#projects-section">Projects</a>
             </li>
-            <li>
+            {/* <li>
               <a href="#sponsors-section">Sponsors</a>
-            </li>
+            </li> */}
             <li>
               <a href="#faq-section">FAQ</a>
             </li>
