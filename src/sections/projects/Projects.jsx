86a43ff88f9a6820f1fc51f0aca9e9ae38b72ddf
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2026-01-18 13:31:02 -0800

    Fixed content

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -6,7 +6,7 @@ export default function Projects({ projectData }) {
     <section id="projects-section">
       <h2>Projects</h2>
       <div className="project-container">
-        {projectData.map((p) => ( 
+        {projectData.map((p) => (
           <Card id={p.id} data={p} type="project" />
         ))}
       </div>
