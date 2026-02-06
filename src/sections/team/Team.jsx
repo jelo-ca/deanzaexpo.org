1f1234fa00930f5507216a67a68706ca6a4e64a3
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2026-02-06 09:25:41 -0800

    Team section mapping

--- src/sections/team/Team.jsx
+++ src/sections/team/Team.jsx
@@ -3,6 +3,7 @@ import "./Team.css";
 
 function FilteredMap({data, team}) {
   return data
+    .toReversed()
     .filter((o) => o.team === team)
     .map((o) => <Card id={o.id} data={o} type="team" />);
 }
@@ -12,23 +13,22 @@ export default function Team({organizerData}) {
     <section id="team-section">
       <h2>Team</h2>
       <div className="team-container">
-        <div className="executive-team-container">
-          <FilteredMap data={organizerData} team="Executive" />
-        </div>
-        <div className="pillars-container">
-          <div className="product-container">
+          <div className="executive-team-container pillar">
+            <h3>Outreach Team</h3>
+            <FilteredMap data={organizerData} team="Executive" />
+          </div>
+          <div className="product-container pillar">
             <h3>Product Team</h3>
             <FilteredMap data={organizerData} team="Product" />
           </div>
-          <div className="programs-container">
+          <div className="programs-container pillar">
             <h3>Programs Team</h3>
             <FilteredMap data={organizerData} team="Programs" />
           </div>
-          <div className="operations-container">
+          <div className="operations-container pillar">
             <h3>Operations Team</h3>
             <FilteredMap data={organizerData} team="Operations" />
           </div>
-        </div>
       </div>
     </section>
   );
