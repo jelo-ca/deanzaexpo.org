a1576de8a69b4696009d469b1c51ad6c9cfee116
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2026-02-06 23:50:46 -0800

    Admin Dashboard Refactor

--- src/sections/team/Team.jsx
+++ src/sections/team/Team.jsx
@@ -13,22 +13,22 @@ export default function Team({ organizerData }) {
     <section id="team-section">
       <h2>Team</h2>
       <div className="team-container">
-          <div className="executive-team-container pillar">
-            <h3>Outreach Team</h3>
-            <FilteredMap data={organizerData} team="Executive" />
-          </div>
-          <div className="product-container pillar">
-            <h3>Product Team</h3>
-            <FilteredMap data={organizerData} team="Product" />
-          </div>
-          <div className="programs-container pillar">
-            <h3>Programs Team</h3>
-            <FilteredMap data={organizerData} team="Programs" />
-          </div>
-          <div className="operations-container pillar">
-            <h3>Operations Team</h3>
-            <FilteredMap data={organizerData} team="Operations" />
-          </div>
+        <div className="executive-team-container pillar">
+          <h3>Outreach Team</h3>
+          <FilteredMap data={organizerData} team="Executive" />
+        </div>
+        <div className="product-container pillar">
+          <h3>Product Team</h3>
+          <FilteredMap data={organizerData} team="Product" />
+        </div>
+        <div className="programs-container pillar">
+          <h3>Programs Team</h3>
+          <FilteredMap data={organizerData} team="Programs" />
+        </div>
+        <div className="operations-container pillar">
+          <h3>Operations Team</h3>
+          <FilteredMap data={organizerData} team="Operations" />
+        </div>
       </div>
     </section>
   );
