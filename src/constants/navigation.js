a27dd464d89c6c48660dc362c18cfa6bca6ec821
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2026-03-17 11:34:16 -0700

    feat(nav): update navigation data

--- src/constants/navigation.js
+++ src/constants/navigation.js
@@ -1,5 +1,9 @@
 export const NAV_LINKS = [
-  { href: "#about-section",    label: "About"    },
-  { href: "#projects-section", label: "Projects" },
-  { href: "#faq-section",      label: "FAQ"      },
+  { href: "#about-section",     label: "About"     },
+  { href: "#timeline-section",  label: "Timeline"  },
+  { href: "#speakers-section",  label: "Speakers"  },
+  { href: "#projects-section",  label: "Projects"  },
+  { href: "#team-section",      label: "Team"      },
+  { href: "#sponsors-section",  label: "Sponsors"  },
+  { href: "#faq-section",       label: "FAQ"       },
 ];
